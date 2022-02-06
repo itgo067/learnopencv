f69c191592804cc2da3e5fc15a01a3d4cae0bd99
itgogogogo <wbf1013@gmail.com>
2022-02-06 16:15:26 +0800

    'init'

---
+++ LearnOpenCV/frmMain.cs
@@ -0,0 +1,15 @@
+namespace LearnOpenCV
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
